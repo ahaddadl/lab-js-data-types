477acb427db0c07d43b179a33785e2a7cef18d7e
ahaddadl <ahaddad@vocento.com>
2024-09-21 14:17:06 +0200

    Lab completado

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s2} ${s1} ${s4}` 
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister)
 
 
 /*******************************************
@@ -22,12 +23,15 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+lastLetter1 = part1[part1.length-1].toLocaleUpperCase()
+lastLetter2 = part2[part2.length-1].toLocaleUpperCase()
 
-
+firstLettersPart1 = part1.slice(0,part1.length-1)
+firstLettersPart2 = part2.slice(0,part2.length-1)
+const result = `${firstLettersPart1}${lastLetter1}${firstLettersPart2}${lastLetter2}`
 // Print the cameLtaiL-formatted string
 
-
-
+console.log(result)
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -36,10 +40,10 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
-
+const tip = 0.15*billTotal
 // Print out the tipAmount
 
-
+console.log(tip)
 
 
 /*******************************************
@@ -48,10 +52,10 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
+const random = Math.ceil(Math.random()*10) 
 // Print the generated random number
 
-
+console.log(random)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +66,24 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//fale
 const expression2 = a || b;
-
+//true
 const expression3 = !a && b;
-
+//false
 const expression4 = !(a && b);
-
+//true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
+
+console.log('1',a && b)
+console.log('2',a || b)
+console.log('3',!a && b)
+console.log('4',!(a && b))
+console.log('5',!a || !b)
+console.log('6',!(a || b))
+console.log('7',a && a)
